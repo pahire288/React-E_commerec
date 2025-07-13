b83b145ebf81154f714573f71c8fa37fe18e5151
Pratik Pahire <pahire288@gmail.com>
2025-07-13 12:27:24 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Products from './components/Producr';
 
 function App() {
   return (
    <>
    
    <Header />
+   <Products />
    </>
   );
 }
---
+++ src/components/Producr.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const productsArr = [
+  {
+    title: 'Colors',
+    price: 100,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+  },
+  {
+    title: 'Black and white Colors',
+    price: 50,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+  },
+  {
+    title: 'Yellow and Black Colors',
+    price: 70,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+  },
+  {
+    title: 'Blue Color',
+    price: 100,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+  },
+];
+
+function Products() {
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        {productsArr.map((product, index) => (
+          <div className="col-md-3 mb-4" key={index}>
+            <div className="card h-100">
+              <img
+                src={product.imageUrl}
+                className="card-img-top"
+                alt={product.title}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">${product.price}</p>
+                <button className="btn btn-primary">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Products;
