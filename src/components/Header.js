e99d0934d0e8588ba255929104b06523d1375f51
Pratik Pahire <pahire288@gmail.com>
2025-07-13 12:23:12 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            The Generics
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Store
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+
+            <button className="btn btn-outline-light">
+              Cart <span className="badge bg-light text-dark">0</span>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
